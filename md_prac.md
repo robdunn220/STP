1fd4a69efecfe334ec0fdfffb7f36856802578b2
robdunn220 <robertdunn220@gmail.com>
2019-04-18 20:14:53 -0400

    Practice with common interview problems/ algorithms

--- md_prac.md
+++ md_prac.md
@@ -217,3 +217,21 @@
 - It is considered a last-in-first-out data structure
 ## Queue
 - Opposite of a stack: first-in-first-out data structure
+
+# **Ch. 22: Algorithms**
+## FizzBuzz
+- Popular interview question
+- Write a program that prints the numbers from 1 to 100. For multiples of three, print "Fizz". For multiples of five print "Buzz". For multiples of both, print "FizzBuzz"
+
+  ```python
+  def fizz_buzz():
+    for i in range(1, 101):
+      if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+      elif i % 3 ==0:
+        print('Fizz')
+      elif i % 5 ==0:
+        print('Buzz')
+      else:
+        print(i)
+  ```
--- stp.py
+++ stp.py
@@ -1,27 +1,38 @@
-# Stacks
+# FizzBuzz
+# def fizz_buzz():
+#     for i in range(1, 101):
+#       if i % 3 == 0 and i % 5 == 0:
+#         print('FizzBuzz')
+#       elif i % 3 ==0:
+#         print('Fizz')
+#       elif i % 5 ==0:
+#         print('Buzz')
+#       else:
+#         print(i)
+#
+# fizz_buzz()
 
-class Stack:
-    """docstring for Stack."""
-    def __init__(self):
-        self.items = []
+# Sequential Search
+# def ss(num_list, n):
+#     found = False
+#     for i in num_list:
+#         if i == n:
+#             found = True
+#             break
+#     return found
 
-    def is_empty(self):
-        return self.items == []
+# Palindrome
+# def palindrome(word):
+#     word = word.lower()
+#     return word[::-1] == word
+#
+# print(palindrome('Mom'))
 
-    def push(self, item):
-        self.items.append(item)
+# Anagram
+def anagram(w1, w2):
+    w1 = w1.lower()
+    w2 = w2.lower()
+    return sorted(w1) == sorted(w2)
 
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        last = len(self.items)-1
-        return self.items[last]
-
-    def size(self):
-        return len(self.items)
-
-st = Stack()
-
-st.push(2)
-print(st.peek())
+print(anagram('iceman', 'cinema'))
+print(anagram('wumbo', 'mumbo'))
